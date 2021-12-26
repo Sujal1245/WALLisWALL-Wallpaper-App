fd50e033eebfb7e856fba26161d6da1e262f38f9
Sujal Kumar <37765241+Sujal1245@users.noreply.github.com>
2021-12-26 10:36:36 +0530

    Hmm...

--- app/src/main/java/com/Sujal_Industries/wallpapers/WALLisWALL/SplashScreen.java
+++ app/src/main/java/com/Sujal_Industries/wallpapers/WALLisWALL/SplashScreen.java
@@ -10,6 +10,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("Hahahahaha");
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
